b70c89767ac6bb4608b757014a3ca2b14a67efdb
Higor Robert Barbist <higor.barbist16@gmail.com>
2025-08-13 10:15:51 -0300

    code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,29 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("O Frete do seu produto: " + CalcularFrete(50,2));
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+    public static double CalcularFrete(double subtotal, double distancia){
+        double frete = 0.0;
+        if (subtotal >= 100){
+            frete = 0;
         }
+        else{
+            if (distancia <= 3){
+                //ERRO PROPOSITAL
+                frete = 5.99;
+            }
+            else if (distancia > 3 && distancia <=6){
+                frete = 9.99;
+            }
+            else if (distancia > 6) {
+                frete = 14.99;
+            }
+        }
+        return frete;
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import org.example.Main;
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+
 
 public class MainTest {
+    @Test
+    public void testMain(){
+        //entrada
+        double subtotal = 50;
+        double distancia = 2;
+
+
+        //saida esperada
+        double esperado = 6.99;
+
+
+        //execucao
+        double resultado = Main.CalcularFrete(subtotal, distancia);
+
+
+        //validacao
+        assertEquals("O frete calculado esta incorreto", esperado, resultado, 0.0001);
+    }
 }
